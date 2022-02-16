53a6aab3ea0acf042a1d50135883c1ae0c1acba0
karen cofre <karencofrecejas@gmail.com>
2022-02-15 22:25:33 -0300

    Initial Commit

---
+++ pokeDex/pokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeDexApp.swift
+//  pokeDex
+//
+//  Created by karen cofré on 15-02-22.
+//
+
+import SwiftUI
+
+@main
+struct pokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
